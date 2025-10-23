e1ab06ede498f56895602d62a54763a27053edd2
Vishal Chaudhary <142567257+vishalnandgaon@users.noreply.github.com>
2025-10-23 10:44:35 +0530

    updated leftsidebar , rightsidebar,profile uadate page

--- Real-time-chatting-app/src/pages/ProfileUpdate/ProfileUpdate1.jsx
+++ Real-time-chatting-app/src/pages/ProfileUpdate/ProfileUpdate1.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
+
+  const [image,setImage]=useState(false);
   return (
-    <div>
-      We are on the Profile update page.
+    <div className='profile'>
+      <div className="profile-container">
+        <form action="">
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input onChange={(e)=> setImage(e.target.files[0])} type="file" id='avatar' accept='.png,.jpg,.jpeg' hidden />
+            <img src={image? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
+            upload profile image
+          </label>
+          <input type="text" placeholder='Your name ' required />
+          <textarea placeholder='write profile bio' required></textarea>
+          <button type='submit'>Save</button>
+        </form>
+        <img className='profile-pic' src={ image? URL.createObjectURL(image) : assets.logo_icon} alt="" />
+      </div>
     </div>
   )
 }
